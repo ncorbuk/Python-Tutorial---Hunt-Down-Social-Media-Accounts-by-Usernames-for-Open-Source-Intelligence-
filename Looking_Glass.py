617d9c6ac73384db5e9048e80a5b7fcdbab66264
Guest257351 <57662212+Guest257351@users.noreply.github.com>
2022-08-24 14:38:58 +0930

    added threading

--- Looking_Glass.py
+++ Looking_Glass.py
@@ -4,6 +4,7 @@
 '''imports'''
 import requests
 import time
+import threading
 
 ''' INPUT USERNMAE TO DOX '''
 username = input('\033[92m{+} Enter username to DOX: ')
@@ -300,9 +301,13 @@ def search():
     time.sleep(1)
 
     count = 0
-    match = True
-    for url in WEBSITES:
-        r = requests.get(url)
+    threads = []
+    results = [False for i in range(len(WEBSITES))]
+    for i in range(len(WEBSITES)):
+        url = WEBSITES[i]
+        def thread(results, index, url):
+            try:
+                r = requests.get(url, timeout=10)
 
         if r.status_code == 200:
             if match == True:
@@ -313,7 +318,23 @@ def search():
                 GREEN(f'POSITIVE MATCH: Username:{username} - text has been detected in url.')
             else:
                 GREEN(f'POSITIVE MATCH: Username:{username} - \033[91mtext has NOT been detected in url, could be a FALSE POSITIVE.')#
-        count += 1
+                    results[index] = True
+            except requests.exceptions.ConnectTimeout:
+                RED(f'{url} - Connection Timeout')
+            except Exception as e:
+                RED(f'ERROR checking {url} - {e}')
+            
+        t = threading.Thread(target=thread, args=(results, i, url))
+        t.start()
+        threads.append(t)
+
+    for thread in threads:
+        thread.join()
+    
+    for i in range(len(results)):
+        if results[i]:
+            count += 1
+    
 
     total = len(WEBSITES)
     GREEN(f'FINISHED: A total of {count} MATCHES found out of {total} websites.')
